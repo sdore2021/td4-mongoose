319da4799b0527b793b83653e58b42b865ab0548
s. dore <samouka.dore@insa-cvl.fr>
2020-02-14 12:23:11 +0100

    fin du td4-mongodb

--- ProduitsApp/controllers/produit.controller.js
+++ ProduitsApp/controllers/produit.controller.js
@@ -1,34 +1,57 @@
-const Produit = require('../models/produit.model');
-
+const Produit = require("../models/produit.model");
+//console.log(Produit);
 // test
-exports.test = function(req,res){
-    res.send('Hello from de Test controller !');
-}
+exports.test = function(req, res) {
+  res.send("Hello from de Test controller !");
+};
 
-exports.produit_create = function(req,res,next){
-    let produit = new Produit({
-        name : req.body.name,
-        price : req.body.price
-    });
+exports.produit_create = function(req, res, next) {
+  let produit = new Produit({
+    name: req.body.name,
+    price: req.body.price
+  });
 
-    produit.save(function(err){
-        if(err){
-            return next(err);
-        }
-        res.send('Produit created');
-    })
-    res.send('Hello from de Test controller !');
-}
+  produit.save(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.send("Produit created");
+  });
+  res.send("Hello from de Test controller !");
+};
 
-exports.produit_details = function(req,res){
-    Produit.findById(req.params.id,function(err,produit){
-        if(err) return next(err);
-        res.send(produit);
-    })
-    res.send('Hello from de Test controller !');
-}
+exports.produit_details = function(req, res) {
+  Produit.findById(req.params.id, function(err, produit) {
+    if (err) return next(err);
+    res.send(produit);
+  });
+};
 
-exports.produit_affiche_all = function(req,res){
-    //res.send(Produit.find()); je veux affiher toute la base de donnée ici
-    res.send('Hello from de Test controller !');
-}
+exports.produit_details = function(req, res) {
+  Produit.findById(req.params.id, function(err, produit) {
+    if (err) return next(err);
+    res.send(produit);
+  });
+};
+
+exports.test_affiche = function(req, res) {
+  Produit.find(function(err, produit) {
+    if (err) return next(err);
+    res.send(produit);
+  });
+};
+
+exports.produit_update = function(req, res) {
+  //console.log(req.body); // le contenu de mettre
+  Produit.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+    if (err) res.send(err);
+    res.json(post);
+  });
+};
+
+exports.produit_delete = function(req, res) {
+  Produit.findByIdAndDelete(req.params.id, function(err, produit) {
+    if (err) res.send(err);
+    res.json(produit);
+  });
+};
--- ProduitsApp/routes/produit.route.js
+++ ProduitsApp/routes/produit.route.js
@@ -1,13 +1,14 @@
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
 
-const produit_controller = require('../controllers/produit.controller');
+const produit_controller = require("../controllers/produit.controller");
 
 // une route pour tester seulement
-router.get('/test',produit_controller.test);
-router.post('/create',produit_controller.produit_create);
-router.get('/:id',produit_controller.produit_details);
-router.get('/affiche',produit_controller.produit_affiche_all);
+router.get("/test", produit_controller.test);
+router.get("/all", produit_controller.test_affiche);
+router.post("/create", produit_controller.produit_create);
+router.get("/:id", produit_controller.produit_details);
+router.put("/:id", produit_controller.produit_update);
+router.delete("/:id", produit_controller.produit_delete);
 
 module.exports = router;
-
